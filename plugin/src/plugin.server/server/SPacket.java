50c1233235f71a80e523e55c7e9297cbf0430943
YoItzPhoenx <147290884+yoitzphoenx@users.noreply.github.com>
2023-10-09 17:18:30 -0700

    Create SPacket.java

---
+++ plugin/src/plugin.server/server/SPacket.java
@@ -0,0 +1,27 @@
+package plugin.server.packets.server;
+
+import net.minecraft.server.v1_8_R3.PacketDataSerializer;
+import net.minecraft.server.v1_8_R3.PacketListener;
+import plugin.server.packets.ECPacket;
+
+public class SPacket extends ECPacket {
+
+	@Override
+	public void readPacketData(PacketDataSerializer data) {
+		// Sent from server
+		
+	}
+
+	@Override
+	public void handle(PacketListener listener) {
+		// Sent from server
+		
+	}
+
+	@Override
+	public void writePacketData(PacketDataSerializer data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
