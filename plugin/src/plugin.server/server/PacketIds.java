3eb6e714b16f6658390778ef5ac2eeea8d3bec14
YoItzPhoenx <147290884+yoitzphoenx@users.noreply.github.com>
2023-10-09 17:17:21 -0700

    Create PacketIds.java

---
+++ plugin/src/plugin.server/server/PacketIds.java
@@ -0,0 +1,10 @@
+package plugin.server.packets.server;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PacketIds extends JavaPlugin implements Listener {
+
+	public static final int SPacketTest = 74;
+	
+}
