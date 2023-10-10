e00846b3477461c0ade476111eb83bb0f4d02951
YoItzPhoenx <147290884+yoitzphoenx@users.noreply.github.com>
2023-10-09 17:17:56 -0700

    Create SPacketTest.java

---
+++ plugin/src/plugin.server/server/SPacketTest.java
@@ -0,0 +1,12 @@
+package plugin.server.packets.server;
+
+import net.minecraft.server.v1_8_R3.PacketDataSerializer;
+
+public class SPacketTest extends SPacket {
+
+	@Override
+	public void writePacketData(PacketDataSerializer data) {
+		// No data read or sent
+	}	
+  
+}
